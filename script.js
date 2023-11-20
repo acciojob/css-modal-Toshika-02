23f1f33cc24f28c05d0b22f62536e39539a91bfb
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2023-11-20 22:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('myModal').style.display = 'block';
+});
+
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById('myModal')) {
+    document.getElementById('myModal').style.display = 'none';
+  }
+};
+
